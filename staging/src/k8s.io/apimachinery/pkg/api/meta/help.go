af4eea4635be77054b33fe84b90b2a9a3fc62897
scott <scottwangsxll@gmail.com>
2022-11-05 21:53:35 +0800

    Add alloc variants

--- staging/src/k8s.io/apimachinery/pkg/api/meta/help.go
+++ staging/src/k8s.io/apimachinery/pkg/api/meta/help.go
@@ -143,6 +143,67 @@ func EachListItem(obj runtime.Object, fn func(runtime.Object) error) error {
 		if takeAddr {
 			raw = raw.Addr()
 		}
+		// raw must be a pointer
+		// allocate a pointer is cheap
+		switch item := raw.Interface().(type) {
+		case *runtime.RawExtension:
+			if err := fn(item.Object); err != nil {
+				return err
+			}
+		case runtime.Object:
+			if err := fn(item); err != nil {
+				return err
+			}
+		default:
+			obj, ok := item.(runtime.Object)
+			if !ok {
+				return fmt.Errorf("%v: item[%v]: Expected object, got %#v(%s)", obj, i, raw.Interface(), raw.Kind())
+			}
+			if err := fn(obj); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// EachListItemWithAlloc invokes fn on each runtime.Object in the list.
+// the difference from  EachListItem is that an object of the same type is regenerated and assigned a value,
+// so that the returned Object list and obj have independent life cycles.
+func EachListItemWithAlloc(obj runtime.Object, fn func(runtime.Object) error) error {
+	if unstructured, ok := obj.(runtime.Unstructured); ok {
+		return unstructured.EachListItem(fn)
+	}
+	// TODO: Change to an interface call?
+	itemsPtr, err := GetItemsPtr(obj)
+	if err != nil {
+		return err
+	}
+	items, err := conversion.EnforcePtr(itemsPtr)
+	if err != nil {
+		return err
+	}
+	len := items.Len()
+	if len == 0 {
+		return nil
+	}
+	takeAddr := false
+	if elemType := items.Type().Elem(); elemType.Kind() != reflect.Pointer && elemType.Kind() != reflect.Interface {
+		if !items.Index(0).CanAddr() {
+			return fmt.Errorf("unable to take address of items in %T for EachListItem", obj)
+		}
+		takeAddr = true
+	}
+
+	for i := 0; i < len; i++ {
+		raw := items.Index(i)
+		if takeAddr {
+			// shallow copy to avoid retaining a reference to the original list item
+			new := reflect.New(raw.Type())
+			new.Elem().Set(raw)
+			// reflect.New will guarantee that new must be a pointer.
+			raw = new
+		}
 		switch item := raw.Interface().(type) {
 		case *runtime.RawExtension:
 			if err := fn(item.Object); err != nil {
@@ -196,6 +257,50 @@ func ExtractList(obj runtime.Object) ([]runtime.Object, error) {
 		default:
 			var found bool
 			if list[i], found = raw.Addr().Interface().(runtime.Object); !found {
+				return nil, fmt.Errorf("%v: item[%v]: Expected object, got %#v(%s)", obj, i, raw.Interface(), raw.Kind())
+			}
+		}
+	}
+	return list, nil
+}
+
+// ExtractListWithAlloc returns obj's Items element as an array of runtime.Objects.
+// the difference from ExtractList is that an object of the same type is regenerated and assigned a value,
+// so that the returned Object list and obj have independent life cycles.
+// Returns an error if obj is not a List type (does not have an Items member).
+func ExtractListWithAlloc(obj runtime.Object) ([]runtime.Object, error) {
+	itemsPtr, err := GetItemsPtr(obj)
+	if err != nil {
+		return nil, err
+	}
+	items, err := conversion.EnforcePtr(itemsPtr)
+	if err != nil {
+		return nil, err
+	}
+	list := make([]runtime.Object, items.Len())
+	for i := range list {
+		raw := items.Index(i)
+		switch {
+		case raw.Type() == rawExtensionObjectType:
+			item := raw.Interface().(runtime.RawExtension)
+			switch {
+			case item.Object != nil:
+				list[i] = item.Object
+			case item.Raw != nil:
+				// TODO: Set ContentEncoding and ContentType correctly.
+				list[i] = &runtime.Unknown{Raw: item.Raw}
+			default:
+				list[i] = nil
+			}
+		case raw.Type().Implements(objectType):
+			list[i] = raw.Interface().(runtime.Object)
+		default:
+			// shallow copy to avoid retaining a reference to the original list item
+			new := reflect.New(raw.Type())
+			// assign to list and type-assert
+			new.Elem().Set(raw)
+			var ok bool
+			if list[i], ok = new.Interface().(runtime.Object); !ok {
 				return nil, fmt.Errorf("%v: item[%v]: Expected object, got %#v(%s)", obj, i, raw.Interface(), raw.Kind())
 			}
 		}
